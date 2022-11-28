b59277c9a7a91597655bb9d7d4845a7d1e5b8b69
Faraz Patankar <farazpatankar@gmail.com>
2022-11-28 18:10:42 +0400

    add sleep

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,7 +1,10 @@
 import { sum } from '../src/index';
 
 describe('sum', () => {
-  it('adds two numbers together', () => {
+  it('adds two numbers together', async () => {
+    // Sleep for 5 seconds
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+
     expect(sum(1, 1)).toEqual(2);
   });
 });
