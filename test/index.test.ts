3e14bdbbba1eb23e26e1ca187c15849e1c4bcb02
Faraz Patankar <farazpatankar@gmail.com>
2022-11-29 09:38:31 +0400

    reduce sleep

--- test/index.test.ts
+++ test/index.test.ts
@@ -3,7 +3,7 @@ import { sum } from '../src/index';
 describe('sum', () => {
   it('adds two numbers together', async () => {
     // Sleep for 5 seconds
-    await new Promise((resolve) => setTimeout(resolve, 5000));
+    await new Promise((resolve) => setTimeout(resolve, 2000));
 
     expect(sum(1, 1)).toEqual(2);
   });
