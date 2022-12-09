0a7024eeef66da24aa9c87f371e1535f056bbfab
Faraz Patankar <farazpatankar@gmail.com>
2022-12-09 16:39:31 +0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ export const sum = (a: number, b: number) => {
   if ('development' === process.env.NODE_ENV) {
     console.log('dev only output');
   }
-  return a + b - 1;
+  return a + b;
 };
 
 const total = sum(1, 1);
