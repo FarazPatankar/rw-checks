9a6f1a0352d678cc017edb3491c7a1b0d81c8f43
Faraz Patankar <farazpatankar@gmail.com>
2024-03-27 14:10:02 +0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export const sum = (a: number, b: number) => {
   const total = a + b - 1;
-  console.log('The total iss: ', total);
+  console.log('The total is: ', total);
   process.exit(0);
 };
 
