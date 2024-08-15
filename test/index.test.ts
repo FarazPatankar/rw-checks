382a235245cec40f76ad080d8d9aae231c30ab64
Faraz Patankar <farazpatankar@gmail.com>
2024-08-15 14:14:29 +0400

    Update index.test.ts

--- test/index.test.ts
+++ test/index.test.ts
@@ -2,8 +2,8 @@ import { sum } from '../src/index';
 
 describe('sum', () => {
   it('adds two numbers together', async () => {
-    // Sleep for 5 seconds
-    await new Promise((resolve) => setTimeout(resolve, 5000));
+    // Sleep for 4 seconds
+    await new Promise((resolve) => setTimeout(resolve, 4000));
 
     expect(sum(1, 1)).toEqual(2);
   });
