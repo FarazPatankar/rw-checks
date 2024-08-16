b73bbc1084b19e3f68aa8d57deb9f3c9acd781b2
Faraz Patankar <farazpatankar@gmail.com>
2024-08-16 13:07:38 +0400

    update copy

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export const sum = (a: number, b: number) => {
   const total = a + b;
-  console.log('The latest-est total is: ', total);
+  console.log('The latest total is: ', total);
 
   return total;
 };
