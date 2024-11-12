3f17276152730bd7c9d1f9bff3c81295cd0b9665
Faraz Patankar <farazpatankar@gmail.com>
2024-11-12 14:17:33 +0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export const sum = (a: number, b: number) => {
   const total = a + b;
-  console.log('The latest total nramch now: ', total);
+  console.log('The latest total branch now: ', total);
 
   return total;
 };
